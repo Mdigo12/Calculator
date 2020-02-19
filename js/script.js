a4aa942c98beeffeed6bd3511c171e4fb2b14eb6
Mdigo12 <mdigojacob@gmail.com>
2020-02-19 20:21:15 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var fNum  = parseInt(prompt('Enter First Number'));
+var sNum = parseInt(prompt('Enter Second Number'));
+
+function add(fNum, sNum) {
+  return fNum + sNum;
+};
+
+alert( 'The sum is' +' ' + add(fNum, sNum));
