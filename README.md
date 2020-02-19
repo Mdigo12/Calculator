e9bff33a8ed4800a667c095af3018a7ae39261fb
Mdigo12 <mdigojacob@gmail.com>
2020-02-19 23:05:50 +0300

    added the functions and a readme

---
+++ README.md
@@ -0,0 +1,1 @@
+The calculator works and if you want to perform an operation and see the result call myValue function and pass in the parameters say sum, diff, product, quotient, or modulous depending on the operation you want to perform.
--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
-var fNum  = parseInt(prompt('Enter First Number'));
+var fNum = parseInt(prompt('Enter First Number'));
 var sNum = parseInt(prompt('Enter Second Number'));
+
+var sum, diff, product, quotient, modulous;
+function myValue(value) {
+  alert(value);
+};
 
 function add(fNum, sNum) {
   return fNum + sNum;
 };
+sum = ('The sum is' + ' ' + add(fNum, sNum));
 
-alert( 'The sum is' +' ' + add(fNum, sNum));
+function dif(fNum, sNum) {
+  return fNum - sNum;
+};
+diff = ('The difference is' + ' ' + dif(fNum, sNum));
+myValue(diff)
+
+function mult(fNum, sNum) {
+  return fNum * sNum;
+};
+product = ('The product is' + ' ' + mult(fNum, sNum));
+
+function quot(fNum, sNum) {
+  return fNum / sNum;
+};
+quotient = ('The quotient is' + ' ' + quot(fNum, sNum));
+
+function mod(fNum, sNum) {
+  return fNum % sNum;
+};
+modulous = ('The reminder is' + ' ' + mod(fNum, sNum));
+
